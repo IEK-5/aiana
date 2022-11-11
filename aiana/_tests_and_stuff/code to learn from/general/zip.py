4a0b00e36a0e3557a023e7e43b93274315648cc2
Leo_simPC <l.raumann@fz-juelich.de>
2022-11-11 11:09:38 +0100

    cleaned repo

--- aiana/_tests_and_stuff/code to learn from/general/zip.py
+++
@@ -1,6 +0,0 @@
-# #
-foo = (range(101))
-bar = reversed(foo)
-
-for letter, number in zip(foo, bar):
-    print(letter, '->', number)
--- aiana/_tests_and_stuff/google_scholar_citation_report.py
+++
@@ -1,6 +0,0 @@
-# #
-import pandas as pd
-from gspread_pandas import Spread
-
-df: pd.DataFrame = Spread('apv_refs').sheet_to_df()
-df['Year'].value_counts().sort_index().plot.bar()
